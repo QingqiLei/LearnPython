bd23857f9577b94ab43821166ce33fefdf5a043e
Chauncey <qingqi_lei@icloud.com>
2018-12-08 19:31:07 +0800

    slice in list tuple string

---
+++ PythonFundamentals/AdvancedFeature/slice.py
@@ -0,0 +1,20 @@
+L = ['Michael','Sarah','Tracy','Bob','Jack']
+L[0:3] # include and exclude
+L[-1] # the last one
+
+L[-3: -1] # the last third  to the last first, in and ex
+
+L = list(range(100))
+L[-10]
+L[10:20]
+L[10:20:2]
+L[::5]
+L[:]
+
+L = (1,2,3,4,5)
+L[::2]
+
+# slice in string
+"abcdefg"[:3]
+
+"abcdefg"[::2]
