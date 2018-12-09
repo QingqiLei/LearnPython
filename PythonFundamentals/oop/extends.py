25be6336d9534f77ea6dacfff107795068869d65
Chauncey <qingqi_lei@icloud.com>
2018-12-09 22:10:54 +0800

    different from java

--- PythonFundamentals/oop/extends.py
+++ PythonFundamentals/oop/extends.py
@@ -2,10 +2,21 @@ class Animal(object):
     def run(self):
         print("Animal is running...")
 class Dog(Animal):
+    def run(self):
+        print("Dog is running")
     pass
 class Cat(Animal):
     pass
 dog = Dog()
-dog.run
+dog.run()
 cat = Cat()
-cat.run
+cat.run()
+isinstance(dog, Dog)
+isinstance(dog, Animal)
+isinstance(dog, object)
+def run_twice(animal): # just to make sure animal has run() method
+    animal.run()
+    animal.run()
+
+run_twice(dog)
+run_twice(cat)
