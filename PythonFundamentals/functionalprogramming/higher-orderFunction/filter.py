aef6ca3ab57d537de6b2b8218460703991117b2d
Chauncey <qingqi_lei@icloud.com>
2018-12-09 16:22:52 +0800

    filter primes palindrome

--- PythonFundamentals/functionalprogramming/higher-orderFunction/filter.py
+++ PythonFundamentals/functionalprogramming/higher-orderFunction/filter.py
@@ -34,6 +34,15 @@ def is_palindrome(n):
     n = str(n)
     m = n[::-1]
     return n==m
+
+def is_palindrome(n):
+    temp = n
+    rev = 0
+    while n != 0:
+        rev = rev * 10 + n % 10
+        n = n //10
+    return rev == temp
+is_palindrome(111)
 output = filter(is_palindrome, range(1,100))
 print(list(output))
 if list(filter(is_palindrome, range(1, 200))) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101, 111, 121, 131, 141, 151, 161, 171, 181, 191]:
