d706df737b292fdc2d3ac14d4bf06e51623907fd
Chauncey <qingqi_lei@icloud.com>
2018-12-09 22:31:28 +0800

    extends and type()

--- PythonFundamentals/oop/extends.py
+++ PythonFundamentals/oop/extends.py
@@ -1,10 +1,14 @@
 class Animal(object):
     def run(self):
         print("Animal is running...")
+    def __len__(self):
+        return 10
 class Dog(Animal):
     def run(self):
         print("Dog is running")
     pass
+    def __len__(self):
+        return 100
 class Cat(Animal):
     pass
 dog = Dog()
@@ -18,6 +22,7 @@ def run_twice(animal): # just to make sure animal has run() method
     animal.run()
     animal.run()
 
+print(dog.__len__())
 run_twice(dog)
 run_twice(cat)
 type(dog) == Dog
