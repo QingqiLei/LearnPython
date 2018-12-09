79ff454913daedeaa4866115626874d6e947f217
Chauncey <qingqi_lei@icloud.com>
2018-12-09 19:04:20 +0800

    use sys _private

---
+++ PythonFundamentals/module/useModule.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'a test module'
+__author__ = "lei"
+import sys
+def test():
+    args = sys.argv
+    if len(args) == 1:
+        print('Hello, World')
+    elif len(args) ==2:
+        print("Hello, %s!" % args[1])
+    else :
+        print('Too many arguemnts!')
+if __name__ == '__main__': # if other python file import this file(useModule), here is false
+    test()
+
+# we assume that method or variable starts with _ is private,
+# we don't call those directly
+def _private_1(name):
+    return 'Hello, %s'%name
+def _private_2(name):
+    return 'Hi, %s'%name
+def greeting(name):
+    if len(name) > 3:
+        return _private_1(name)
+    else:
+        return _private_2(name)
+
+print(greeting('mi'))
+print(greeting('micc'))
