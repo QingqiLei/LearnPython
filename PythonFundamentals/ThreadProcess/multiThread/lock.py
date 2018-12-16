5cad82b0cc7d83650374eb78d0035fd88781bb1d
chauncey <qingqi_lei@icloud.com>
2018-12-16 09:47:20 +0800

    threading.Lock() lock.acquire() lock.release()

--- PythonFundamentals/ThreadProcess/multiThread/lock.py
+++ PythonFundamentals/ThreadProcess/multiThread/lock.py
@@ -15,3 +15,14 @@ t2.start()
 t1.join()
 t2.join()
 print(balance, sep=' ', end='n', file=sys.stdout, flush=False)
+# balance = balance + n   =>
+# x = balance + n
+# balance = x
+lock = threading.Lock()
+def run_thread(n):
+    for i in range(10000):
+        lock.acquire()
+        try:
+            change_it(n)
+        finally:
+            lock.release()
